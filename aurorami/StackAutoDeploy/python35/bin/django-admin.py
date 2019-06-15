0d3b65e52ad7833e211a1441a58e15714280b2cd
aurorami <2972053698@qq.com>
2019-06-16 01:19:43 +0800

    删除Python环境

--- aurorami/StackAutoDeploy/python35/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/user/gitProject/corns/aurorami/StackAutoDeploy/python35/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
